f68a924dd5516a92ad0a6388e1553abb5d5b3a2f
GianlucaPolcino <gianluca.polcino.gp@gmail.com>
2021-11-10 14:46:27 +0100

    html injected with vue

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        message: 'Hello world!',
+        image: 'https://i.pinimg.com/originals/d9/4a/49/d94a495eca526d82ebbe0640aea413a9.jpg',
+    }
+
+})
